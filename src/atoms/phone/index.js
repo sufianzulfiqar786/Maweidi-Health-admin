656cbb95639159e60653f3fde49f8de922a70d1e
Haidar Ali <alihaidar14157@gmail.com>
2023-07-14 16:26:35 +0500

    error rsolve

--- src/atoms/phone/index.js
+++ src/atoms/phone/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import "./Phone.scss";
-const Phone = ({ value, handleChange,name }) => {
+const Phone = ({ value, handleChange, name }) => {
   return (
     <>
       <p className="mb-2">Phone no</p>
@@ -11,7 +11,7 @@ const Phone = ({ value, handleChange,name }) => {
         value={value}
         inputStyle={{ width: "36.6px" }}
         inputClass="w-100"
-        onChange={(phone) => handleChange(phone,name)}
+        onChange={(phone) => handleChange(phone, name)}
       />
     </>
   );
